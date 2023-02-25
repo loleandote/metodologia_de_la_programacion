5c4a9f22fea1451c92544369ced87fa7e31647d6
loleandote <48246750+loleandote@users.noreply.github.com>
2023-02-25 09:28:15 +0100

    Comit edicion

--- src/caso1/Presentacion.java
+++ src/caso1/Presentacion.java
@@ -7,12 +7,12 @@ public class Presentacion {
 		long t0,t1,deltaT;
 		
 		Scanner leer= new Scanner(System.in);
-		System.out.println("Introduce un numero\n");
+		System.out.println("Introduce un numero");
 		int numero=leer.nextInt();
 		
-		t0=System.currentTimeMillis();
+		t0=System.nanoTime();
 		System.out.println("El resultado obtenido resulto a traves de la formula cerrada es: " + clase_negocio.formula_cerrada(numero));
-		t1=System.currentTimeMillis();
+		t1=System.nanoTime();
 		deltaT=t1-t0;
 		System.out.println("El tiempoo usado para la formula cerrada es");
 		try {
@@ -23,11 +23,9 @@ public class Presentacion {
 		}
 		System.out.println(deltaT);
 		
-		t0=System.currentTimeMillis();
-		System.out.println(t0);
+		t0=System.nanoTime();
 		System.out.println("El resultado obtenido resulto a traves de la formula iterativa es: "+clase_negocio.iterativo(numero));
-		t1=System.currentTimeMillis();
-		System.out.println(t1);
+		t1=System.nanoTime();
 		
 		deltaT=t1-t0;
 		System.out.println("El tiempoo usado para la formula iterativa es");
@@ -39,9 +37,9 @@ public class Presentacion {
 		}
 		System.out.println(deltaT);
 		
-		t0=System.currentTimeMillis();
+		t0=System.nanoTime();
 		System.out.println("El resultado obtenido resulto a traves de la formula recursiva es: "+clase_negocio.recursivo(numero));
-		t1=System.currentTimeMillis();
+		t1=System.nanoTime();
 		deltaT=t1-t0;
 		System.out.println("El tiempoo usado para la formula recursiva es");
 		try {
@@ -51,7 +49,7 @@ public class Presentacion {
 			e.printStackTrace();
 		}
 		System.out.println(deltaT);
-		
+		leer.close();
 		
 		
 	}
--- src/caso1/clase_negocio.java
+++ src/caso1/clase_negocio.java
@@ -2,24 +2,49 @@ package caso1;
 
 public class clase_negocio {
 
+	
+	/**
+	 * La función toma un entero como argumento y devuelve el número hexagonal de ese entero
+	 * Presenta una complejidad de O(1)
+	 * 
+	 * @param x El número del número hexagonal que desea encontrar.
+	 * @return El enésimo número hexagonal.
+	 */
 	public static int formula_cerrada(int x) {
-		int n;
-		n=x*(2*x-1);
-		return n;
+		int nHexagonal;
+		nHexagonal = x * (2 * x - 1);
+		return nHexagonal;
 	}
+
+	/**
+	 * La función iterativo() toma un entero x como parámetro y devuelve el número
+	 * hexagonal de x
+	 * Con una complejidad de O(n)
+	 * 
+	 * @param x El número de hexágonos que se agregarán.
+	 * @return El número de mosaicos hexagonales en un hexágono con un lado de
+	 *         longitud x.
+	 */
 	public static int iterativo(int x) {
-		int r=0;
-		for(int i=0;i<x;i++) {
-		r=r+4*i+1;	
-		}
-		return r;
+		int nHexagonal = 0;
+		for (int i = 0; i < x; i++)
+			nHexagonal = nHexagonal + 4 * i + 1;
+		return nHexagonal;
 	}
+
+	/**
+	 * La función se llama recursivamente a sí misma hasta que el valor de x es 1, luego devuelve el valor
+	 * de nHexagonal
+	 * Con una complejidad de O(n)
+	 * 
+	 * @param x El número del número hexagonal que desea encontrar.
+	 * @return El enésimo número hexagonal.
+	 */
 	public static int recursivo(int x) {
-		int r=1;
-		if(x>1) {
-			r=recursivo(x-1)+4*x-3;
-		}
-		return r;
+		int nHexagonal=1;
+		if(x>1) 
+			nHexagonal=recursivo(x-1)+4*x-3;
+		return nHexagonal;
 	}
 
 }
