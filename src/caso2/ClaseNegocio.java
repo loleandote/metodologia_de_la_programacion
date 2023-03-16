82f01bd68f8d49cd5a2f8f16436712dab1105dba
alvaro97 <115146087+agamenom97@users.noreply.github.com>
2023-03-16 18:13:22 +0100

    Caso 2 implentación terminada

---
+++ src/caso2/ClaseNegocio.java
@@ -0,0 +1,46 @@
+package caso2;
+
+public class ClaseNegocio {
+
+	public static int countInversions(double a[], int p, int r) {
+		int veces = 0;
+		if (p < r) {
+			veces++;
+			int mid = (p + r) / 2;
+			veces = countInversions(a, p, mid);
+			veces += countInversions(a, mid + 1, r);
+			veces += MergeAndCounts(a, p, mid, r);
+
+		}
+		return veces;
+	}
+
+	public static int MergeAndCounts(double a[], int p, int q, int r) {
+
+		int i = p, j = q + 1, k = p, veces = 0;
+		int[] temp = new int[r - p + 1];
+		for (int l = p; l <= r; l++)
+			temp[l - p] = (int) a[l];
+
+		while (i <= q && j <= r) {
+			if (temp[i - p] <= temp[j - p]) {
+				a[k] = temp[i - p];
+				i++;
+			} else {
+				a[k] = temp[j - p];
+				j++;
+				veces++;
+			}
+			k++;
+		}
+		if (j - 1 == r) {
+			while (i <= q) {
+				a[k] = temp[i - p];
+				k++;
+				i++;
+			}
+		}
+		return veces;
+	}
+
+}
--- src/caso2/Presentacion.java
+++ src/caso2/Presentacion.java
@@ -1,27 +1,28 @@
 package caso2;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Presentacion {
-	public static void main(String[] args) throws Exception  {
-		//No lo lee
-		double a[]=lectorDat("InversionsTest_6.dat");
-		
-		
-		
-	}
-	
-	
+	public static void main(String[] args) throws Exception {
 
-	
+		double a[] = lectorDat("InversionsTest_6.dat");
+
+		System.out.println(ClaseNegocio.countInversions(a, 0, a.length - 1));
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+	}
+
 	public static double[] lectorDat(String nombre) throws Exception {
-		Scanner read =new Scanner(new File(nombre));
+		Scanner read = new Scanner(new File(nombre));
 		int nCases = read.nextInt();
-		double [ ] a = new double[nCases];
-		for (int i= 0; i< nCases; i++) {
-			a[i] = read.nextInt();
+		double[] a = new double[nCases];
+		for (int i = 0; i < nCases; i++) {
+			a[i] = read.nextDouble();
 		}
 		return a;
 	}
+
 }
