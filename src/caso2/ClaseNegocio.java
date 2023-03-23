5170a854047766801f5ec01dec564efc0965e3a8
loleandote <48246750+loleandote@users.noreply.github.com>
2023-03-23 15:10:49 +0100

    Comentado sin complegidad

--- src/caso2/ClaseNegocio.java
+++ src/caso2/ClaseNegocio.java
@@ -1,11 +1,23 @@
 package caso2;
 
+/**
+ * Toma una matriz de dobles la ordena de menor a mayor y devuelve el número de inversiones en la matriz
+ */
 public class ClaseNegocio {
 
+	/**
+	 * Si la matriz es de tamaño 1, entonces no hay inversiones. De lo contrario, divide la matriz en dos
+	 * mitades, cuenta la cantidad de inversiones en cada mitad y las suma. Luego, cuenta la cantidad de
+	 * inversiones en las dos mitades y lo suma al total
+	 * 
+	 * @param a la matriz a ordenar
+	 * @param p el comienzo de la matriz
+	 * @param r el último índice de la matriz
+	 * @return cantidad de inversiones en la matriz.
+	 */
 	public static int countInversions(double a[], int p, int r) {
 		int veces = 0;
 		if (p < r) {
-			veces++;
 			int mid = (p + r) / 2;
 			veces = countInversions(a, p, mid);
 			veces += countInversions(a, mid + 1, r);
@@ -15,6 +27,16 @@ public class ClaseNegocio {
 		return veces;
 	}
 
+	/**
+	 * Toma una matriz, un indice inicial, un indice medio y un indice final, combina las dos submatrices
+	 * en una matriz ordenada y devuelve el número de inversiones en las submatrices.
+	 * 
+	 * @param a la matriz a ordenar
+	 * @param p el primer índice de la matriz
+	 * @param q el medio de la matriz
+	 * @param r el último índice de la matriz
+	 * @return El número de veces que se ordena la matriz.
+	 */
 	public static int MergeAndCounts(double a[], int p, int q, int r) {
 
 		int i = p, j = q + 1, k = p, veces = 0;
--- src/caso2/Presentacion.java
+++ src/caso2/Presentacion.java
@@ -8,13 +8,19 @@ public class Presentacion {
 	public static void main(String[] args) throws Exception {
 
 		double a[] = lectorDat("InversionsTest_6.dat");
-
+		//double a[]={3,2,1};
 		System.out.println("Inversiones: "+ ClaseNegocio.countInversions(a, 0, a.length - 1)+"\nArray:");
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
 	}
 
+	/**
+	 * Lee un archivo y devuelve una matriz de dobles.
+	 * 
+	 * @param nombre el nombre del archivo que desea leer
+	 * @return El método devuelve una matriz de dobles.
+	 */
 	public static double[] lectorDat(String nombre) throws Exception {
 		Scanner read = new Scanner(new File(nombre));
 		int nCases = read.nextInt();
