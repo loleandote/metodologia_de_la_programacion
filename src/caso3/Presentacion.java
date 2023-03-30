5e6a2cd04e8bf1cfc86b33266b64ad6356d60f3a
Amparo98 <96288249+Amparo98@users.noreply.github.com>
2023-03-30 18:07:01 +0200

    Prueba

--- src/caso3/Presentacion.java
+++ src/caso3/Presentacion.java
@@ -1,7 +1,19 @@
 package caso3;
 
+import java.util.Scanner;
+
 public class Presentacion {
 	public static void main(String[] args) {
+
+		int parcela;
+
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Cuantos metro desea que tenga la parcela");
+		parcela = leer.nextInt();
+		System.out.println("¿Cual es el numero de balsosas que temdremos?");
+		System.out.println("Inserte las dimensiones de las balsosas en m");
+hgvbjnmk,.
+		leer.close();
 		
 	}
 }
