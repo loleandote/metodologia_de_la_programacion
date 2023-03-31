6f860cb9e94056d7d50f8f782306a4a8cdfabedf
loleandote <48246750+loleandote@users.noreply.github.com>
2023-03-31 10:55:34 +0200

    Calcula bloques

--- src/caso3/ClaseNegocio.java
+++ src/caso3/ClaseNegocio.java
@@ -1,5 +1,38 @@
 package caso3;
 
-public class ClaseNegocio {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class ClaseNegocio {
+    public static void devolverBaldosas(double area, List<Double>baldosas)
+    {
+        double areaOriginal= area;
+        Map<Double,Integer>baldosasH=new HashMap<>();
+        //Crear Cuadrado
+        boolean encontrado= false;
+        for(int i=0;i<baldosas.size()&&!encontrado&&area>0;i++)
+        {
+            double baldosa= baldosas.get(i);
+            if(area>=baldosa){
+                int veces=(int)(area/baldosa);
+                baldosasH.put(baldosa, veces);
+                area=area%baldosa;
+                encontrado=true;
+            }
+        }
+        // Rellenar
+        for(int i=0;i<baldosas.size()&&!encontrado&&area>0;i++){
+            double baldosa= baldosas.get(i);
+            if(area>=baldosa){
+                int veces=(int)((area/baldosa*areaOriginal/baldosa)+(area/baldosa*(areaOriginal-area)/baldosa));
+                baldosasH.put(baldosa, veces);
+                area=area%baldosa;
+            }
+        }
+        
+        baldosasH.forEach((key, value) -> System.out.println(key+" "+value));
+        if(area>0)
+            System.out.println("no se puede");
+    }
 }
--- src/caso3/Presentacion.java
+++ src/caso3/Presentacion.java
@@ -1,7 +1,13 @@
 package caso3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Presentacion {
 	public static void main(String[] args) {
-		
+		List<Double>baldosas= new ArrayList<Double>();
+		baldosas.add(2.0);
+		baldosas.add(1.0);
+		ClaseNegocio.devolverBaldosas(3.0, baldosas);
 	}
 }
