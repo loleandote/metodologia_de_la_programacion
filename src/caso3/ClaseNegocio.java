98402960b1f568e39aaf1cd2a3d6e12fce89e4e2
alvaro97 <115146087+agamenom97@users.noreply.github.com>
2023-04-13 15:14:34 +0200

    inializacion de variables negocio

--- src/caso3/ClaseNegocio.java
+++ src/caso3/ClaseNegocio.java
@@ -1,5 +1,17 @@
 package caso3;
+import java.util.Arrays;
 
 public class ClaseNegocio {
-
+	public static int[][] tileFloor(int baldosas[],int n) {
+		Arrays.sort(baldosas);
+		int area=n*n;
+		int suelo[][]=new int [n][n];
+		int posicion=baldosas.length-1;
+		int tamaño=baldosas[posicion];
+		
+		
+		
+			
+		return suelo;
+	}
 }
--- src/caso3/Presentacion.java
+++ src/caso3/Presentacion.java
@@ -1,7 +1,21 @@
 package caso3;
 
+import java.util.Scanner;
+
 public class Presentacion {
 	public static void main(String[] args) {
 		
+
+		Scanner leer = new Scanner(System.in);
+		System.out.println("Introduce cuanto mide el lado del suelo");
+		int lado_suelo = leer.nextInt();
+		int baldosas[] = {1,2,4,5};
+		int solucion[][]=ClaseNegocio.tileFloor(baldosas,lado_suelo);
+		for (int i = 0; i < lado_suelo; i++){
+			System.out.print("\n");
+			for (int j = 0; j < lado_suelo; j++){
+				System.out.print(solucion[i][j]);
+			}
+		}
 	}
 }
